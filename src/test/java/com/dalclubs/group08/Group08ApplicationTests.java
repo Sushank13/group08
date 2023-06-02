299cc5443b805ad9c686b08aa2c44d87bf1c0d46
Sushank Saini <sushank.saini@dal.ca>
2023-06-02 10:37:11 -0300

    checking mirroring of repository

---
+++ src/test/java/com/dalclubs/group08/Group08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalclubs.group08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
