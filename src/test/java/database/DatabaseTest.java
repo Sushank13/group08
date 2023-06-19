091db9829cfc64fc3d31d0395e5f673eb4f68e6a
Om Anand <om.anand@dal.ca>
2023-06-18 23:31:59 -0300

    Removed unused import

--- src/test/java/database/DatabaseTest.java
+++ src/test/java/database/DatabaseTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.xml.crypto.Data;
 import java.sql.Connection;
 
 public class DatabaseTest {
