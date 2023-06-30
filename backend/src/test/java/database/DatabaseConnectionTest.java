c736d7a67d09923d5f501d23ad1ed4c567013c78
Om Anand <om.anand@dal.ca>
2023-06-30 15:28:36 -0300

    Added test to check close database connection

--- backend/src/test/java/database/DatabaseConnectionTest.java
+++ backend/src/test/java/database/DatabaseConnectionTest.java
@@ -1,14 +1,10 @@
 package database;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
 
 public class DatabaseConnectionTest {
     private DatabaseConnection databaseConnection;
@@ -24,4 +20,11 @@ public class DatabaseConnectionTest {
     public void checkCreateDatabaseConnection() throws Exception {
         Assertions.assertTrue(connection.isValid(60));
     }
+
+    @Test
+    public void checkCloseDatabaseConnection() throws Exception {
+        databaseConnection.closeDatabaseConnection();
+        Assertions.assertTrue(connection.isClosed());
+    }
+
 }
