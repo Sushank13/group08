a9941613c819b910d0a82c63576eb4ec68c9382c
Zainuddin Saiyed <zn561051@dal.ca>
2023-07-02 01:00:59 -0300

    Adding Controller Layer function for getting all the categories

--- backend/src/main/java/com/dal/cs/backend/Club/Controller/ClubController.java
+++ backend/src/main/java/com/dal/cs/backend/Club/Controller/ClubController.java
@@ -1,12 +1,12 @@
-package Club.Controller;
+package com.dal.cs.backend.Club.Controller;
 
-import Club.ClassObject.Club;
-import Club.ServiceLayer.IClubServiceLayer;
+import com.dal.cs.backend.Club.ClassObject.Club;
+import com.dal.cs.backend.Club.ServiceLayer.IClubServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @RestController
 public class ClubController
@@ -20,10 +20,17 @@ public class ClubController
     * @return a message to the user with the request id in case request is submitted or an error message
     * if the request is not submitted
     */
-   @RequestMapping(method = RequestMethod.POST,value="/registerClub")
+   @RequestMapping(method = RequestMethod.POST, value="/registerClub")
    public String createClubRequest(@RequestBody Club club)
    {
       String message=iClubServiceLayer.createNewClubRequest(club);
       return message;
+   }
+
+   @RequestMapping(method = RequestMethod.GET, value="/findAllClubsByCategory")
+   public ArrayList<HashMap<String, String>> findClubByCategory() {
+
+      ArrayList<HashMap<String, String>> allClubCategories = iClubServiceLayer.getAllClubCategories();
+      return allClubCategories;
    }
 }
