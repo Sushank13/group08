6e7ece112b39fb1af004b6d2b7684d61e4313c7d
Zainuddin Saiyed <zn561051@dal.ca>
2023-07-15 19:08:45 -0300

    Adding Controller layer function for creating Events in event table.

--- backend/src/main/java/com/dal/cs/backend/Event/Controller/EventController.java
+++ backend/src/main/java/com/dal/cs/backend/Event/Controller/EventController.java
@@ -1,35 +1,42 @@
 package com.dal.cs.backend.Event.Controller;
 import com.dal.cs.backend.Club.Controller.ClubController;
 import com.dal.cs.backend.Event.EventObject.Event;
-import com.dal.cs.backend.Event.ServiceLayer.EventServiceLayer;
 import com.dal.cs.backend.Event.ServiceLayer.IEventServiceLayer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 @CrossOrigin(origins = "http://localhost:3001")
 @RestController
-public class EventController
-{
-    private static final Logger logger= LogManager.getLogger(ClubController.class);
+public class EventController {
+    private static final Logger logger = LogManager.getLogger(ClubController.class);
     @Autowired
     IEventServiceLayer iEventServiceLayer;
+
     /**
      * This receives request to retrieve a list of all events in DalClubs
+     *
      * @return list of events
      */
-    @RequestMapping(method = RequestMethod.GET, value="/getAllEvents")
-    public List<Event> getAllEvents()
-    {
+    @RequestMapping(method = RequestMethod.GET, value = "/getAllEvents")
+    public List<Event> getAllEvents() {
         logger.info("Controller Entered: Received request to get all events.");
         logger.info("getAllEvents- Calling getAllEvents() of ServiceLayer");
-        List<Event> listOfAllEvents=iEventServiceLayer.getAllEvents();
+        List<Event> listOfAllEvents = iEventServiceLayer.getAllEvents();
         logger.info("Exiting Controller: Returning list of events to Frontend via GET /getAllEvents");
         return listOfAllEvents;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/createEvent")
+    public Map<String, Boolean> createEvent(@RequestBody Event event) {
+        Map<String, Boolean> resultResponse = new HashMap<>();
+        boolean result = iEventServiceLayer.createEvent(event);
+        resultResponse.put("status", result);
+        return resultResponse;
     }
 }
