9a3c39ed9fcf8fbcaf1b7a39612a8dbc9fec25b3
Om Anand <om.anand@dal.ca>
2023-07-15 18:56:22 -0300

    [Auth][login] Auto wire member controller constructor

--- backend/src/main/java/com/dal/cs/backend/member/Controller/MemberController.java
+++ backend/src/main/java/com/dal/cs/backend/member/Controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.dal.cs.backend.member.Controller;
 
-import com.dal.cs.backend.member.MemberObject.Member;
 import com.dal.cs.backend.member.MemberObject.MemberWithLoginCredential;
 import com.dal.cs.backend.member.ServiceLayer.MemberServiceLayer;
 import org.apache.logging.log4j.LogManager;
@@ -15,12 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberController {
     private static final Logger logger = LogManager.getLogger(MemberController.class);
-    @Autowired
+
     MemberServiceLayer iMemberServiceLayer;
+
+    @Autowired
+    public MemberController(MemberServiceLayer iMemberServiceLayer) {
+        this.iMemberServiceLayer = iMemberServiceLayer;
+    }
 
     /**
      * This method accepts the member details  for a new member register request.
-     * @param member is the entity to which all the member details submitted by the user are mapped.
+     * @param memberWithLoginCredential is the entity to which all the member details submitted by the user are mapped.
      * @return a message to the user with the request id in case request is submitted or an error message
      * if the request is not submitted
      */
