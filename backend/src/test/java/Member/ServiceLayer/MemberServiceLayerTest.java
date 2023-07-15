054c406c51e2f1b3d26728f9898c4ebc09ac8628
Om Anand <om.anand@dal.ca>
2023-07-15 07:25:31 -0300

    [Auth][login] Added logs to member service test

--- backend/src/test/java/Member/ServiceLayer/MemberServiceLayerTest.java
+++ backend/src/test/java/Member/ServiceLayer/MemberServiceLayerTest.java
@@ -7,14 +7,20 @@ import com.dal.cs.backend.member.DataLayer.IMemberDataLayer;
 import com.dal.cs.backend.member.DataLayer.MemberDataLayer;
 import com.dal.cs.backend.member.MemberObject.MemberWithLoginCredential;
 import com.dal.cs.backend.member.ServiceLayer.MemberServiceLayer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import testUtils.RandomGenerator;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MemberServiceLayerTest {
 
     private MemberServiceLayer memberServiceLayer;
+
+    private static final Logger logger= LogManager.getLogger(MemberServiceLayerTest.class);
 
     @BeforeAll
     public void testSetUp() {
@@ -27,6 +33,7 @@ public class MemberServiceLayerTest {
     @Test
     public void createNewMemberRequestTest() {
         MemberWithLoginCredential newMember = RandomGenerator.generateRandomDalClubMemberWithLoginCredential();
+        logger.info("[Test][Member][Service] Created test member with emailId: "+newMember.getEmailId());
         Assertions.assertNotNull(memberServiceLayer.createNewMemberRequest(newMember));
     }
 }
