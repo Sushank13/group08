1ebe29cea9e7a1d4f4f055df14e22f1b4c9bd88c
Sushank Saini <sushank.saini@dal.ca>
2023-07-21 22:49:17 -0300

    added MemberBuilder class

---
+++ backend/src/main/java/com/dal/cs/backend/member/ObjectBuilder/MemberBuilder.java
@@ -0,0 +1,62 @@
+package com.dal.cs.backend.member.ObjectBuilder;
+
+import com.dal.cs.backend.member.Enum.MemberType;
+import com.dal.cs.backend.member.MemberObject.Member;
+
+import java.time.LocalDate;
+
+public class MemberBuilder
+{
+    private String emailId;
+    private String firstName;
+    private String lastName;
+    private MemberType memberType;
+    private String program;
+    private int term;
+    private String mobile;
+    private LocalDate dob;
+
+    public MemberBuilder setEmailId(String emailId) {
+        this.emailId = emailId;
+        return this;
+    }
+
+    public MemberBuilder setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public MemberBuilder setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public MemberBuilder setMemberType(MemberType memberType) {
+        this.memberType = memberType;
+        return this;
+    }
+
+    public MemberBuilder setProgram(String program) {
+        this.program = program;
+        return this;
+    }
+
+    public MemberBuilder setTerm(int term) {
+        this.term = term;
+        return this;
+    }
+
+    public MemberBuilder setMobile(String mobile) {
+        this.mobile = mobile;
+        return this;
+    }
+
+    public MemberBuilder setDob(LocalDate dob) {
+        this.dob = dob;
+        return this;
+    }
+    public Member createMember()
+    {
+        return new Member(emailId,firstName,lastName,memberType, program,term,mobile,dob);
+    }
+}
