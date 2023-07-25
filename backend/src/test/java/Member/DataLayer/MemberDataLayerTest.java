7875f656400682633fb356adcc29b8499ab0dc4b
Om Anand <om.anand@dal.ca>
2023-07-25 16:18:57 -0300

    [Test][Member][Data] Fixed member data layer test and updated enum method

--- backend/src/test/java/Member/DataLayer/MemberDataLayerTest.java
+++ backend/src/test/java/Member/DataLayer/MemberDataLayerTest.java
@@ -4,7 +4,6 @@ import com.dal.cs.backend.database.DatabaseConnection;
 import com.dal.cs.backend.database.IDatabaseConnection;
 import com.dal.cs.backend.member.DataLayer.IMemberDataLayer;
 import com.dal.cs.backend.member.DataLayer.MemberDataLayer;
-import com.dal.cs.backend.member.Enum.MemberType;
 import com.dal.cs.backend.member.MemberObject.Member;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -38,7 +37,7 @@ public class MemberDataLayerTest {
     @Test
     public void createNewMemberTest() {
         logger.info("[Test][Member][Service] Created test member with emailId: " + randomMember.getEmailId());
-//        Assertions.assertTrue(() -> memberDataLayer.createNewMember(randomMember));
+        Assertions.assertTrue(() -> memberDataLayer.createNewMember(randomMember));
     }
 
     @Test
@@ -46,17 +45,17 @@ public class MemberDataLayerTest {
         logger.info("[Test][Member][Service] Created test member with emailId: " + randomMember.getEmailId());
         memberDataLayer.createNewMember(randomMember);
         Member recievedMember = memberDataLayer.getMember(randomMember.getEmailId());
-//        Assertions.assertNotNull(recievedMember, "Member not found");
-//
-//        //Assert member values
-//        Assertions.assertEquals(randomMember.getEmailId(), recievedMember.getEmailId());
-//        Assertions.assertEquals(randomMember.getFirstName(), recievedMember.getFirstName());
-//        Assertions.assertEquals(randomMember.getLastName(), recievedMember.getLastName());
-//        Assertions.assertEquals(randomMember.getMemberType(), recievedMember.getMemberType());
-//        Assertions.assertEquals(randomMember.getProgram(), recievedMember.getProgram());
-//        Assertions.assertEquals(randomMember.getTerm(), recievedMember.getTerm());
-//        Assertions.assertEquals(randomMember.getMobile(), recievedMember.getMobile());
-//        Assertions.assertEquals(randomMember.getDob(), recievedMember.getDob());
+        Assertions.assertNotNull(recievedMember, "Member not found");
+
+        //Assert member values
+        Assertions.assertEquals(randomMember.getEmailId(), recievedMember.getEmailId());
+        Assertions.assertEquals(randomMember.getFirstName(), recievedMember.getFirstName());
+        Assertions.assertEquals(randomMember.getLastName(), recievedMember.getLastName());
+        Assertions.assertEquals(randomMember.getMemberType(), recievedMember.getMemberType());
+        Assertions.assertEquals(randomMember.getProgram(), recievedMember.getProgram());
+        Assertions.assertEquals(randomMember.getTerm(), recievedMember.getTerm());
+        Assertions.assertEquals(randomMember.getMobile(), recievedMember.getMobile());
+        Assertions.assertEquals(randomMember.getDob(), recievedMember.getDob());
     }
 
     @Test
@@ -64,6 +63,6 @@ public class MemberDataLayerTest {
     public void deleteMemberTest() {
         logger.info("[Test][Member][Service] Created test member with emailId: " + randomMember.getEmailId());
         memberDataLayer.createNewMember(randomMember);
-//        Assertions.assertTrue(() -> memberDataLayer.deleteMember(randomMember.getEmailId()));
+        Assertions.assertTrue(() -> memberDataLayer.deleteMember(randomMember.getEmailId()));
     }
 }
