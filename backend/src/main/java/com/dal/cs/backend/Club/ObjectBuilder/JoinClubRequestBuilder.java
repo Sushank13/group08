0d697f99c1e3b973fcc5f839a25591f8453f80c9
Sushank Saini <sushank.saini@dal.ca>
2023-07-25 16:19:04 -0300

    added join club request builder object

--- backend/src/main/java/com/dal/cs/backend/Club/ObjectBuilder/JoinClubRequestBuilder.java
+++ backend/src/main/java/com/dal/cs/backend/Club/ObjectBuilder/JoinClubRequestBuilder.java
@@ -1,2 +1,35 @@
-package com.dal.cs.backend.Club.ObjectBuilder;public class JoinClubRequestBuilder {
+package com.dal.cs.backend.Club.ObjectBuilder;
+
+public class JoinClubRequestBuilder
+{
+    private String requestID;
+    private String requesterEmailID;
+    private String clubID;
+    private String joiningReason;
+    private String requestStatus;
+
+    public JoinClubRequestBuilder setRequestID(String requestID) {
+        this.requestID = requestID;
+        return this;
+    }
+
+    public JoinClubRequestBuilder setRequesterEmailID(String requesterEmailID) {
+        this.requesterEmailID = requesterEmailID;
+        return this;
+    }
+
+    public JoinClubRequestBuilder setClubID(String clubID) {
+        this.clubID = clubID;
+        return this;
+    }
+
+    public JoinClubRequestBuilder setJoiningReason(String joiningReason) {
+        this.joiningReason = joiningReason;
+        return this;
+    }
+
+    public JoinClubRequestBuilder setRequestStatus(String requestStatus) {
+        this.requestStatus = requestStatus;
+        return this;
+    }
 }
