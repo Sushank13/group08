9bb5d41b2f754018c2430c5a6b5c0b77b2d0c5bd
preetisharmadal <preeti28062000@gmail.com>
2023-07-27 18:30:36 -0300

    added create event form
    Signed-off-by: preetisharmadal <preeti28062000@gmail.com>

--- frontend/src/Pages/CreateEventForm.js
+++ frontend/src/Pages/CreateEventForm.js
@@ -1,20 +1,32 @@
 import { Button, Container, Flex, FormControl, FormLabel, Grid, HStack, Input, Text, useToast } from '@chakra-ui/react';
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 
 
 function CreateEventForm() {
     const toast = useToast();
-    const handleSubmit = (event) => {
+    const [eventData, setEventData] = useState();
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        toast({
-            title: 'Successful',
-            description: 'Your submission was successful.',
-            status: 'success',
-            duration: 5000,
-            isClosable: true,
-        });
+        try {
+            const response = await axios.post(`/president/createEvent`, eventData);
+            if (response.status === 200) {
+                toast({
+                    title: 'Successful',
+                    description: 'Your submission was successful.',
+                    status: 'success',
+                    duration: 5000,
+                    isClosable: true,
+                });
+            }
+            return response.data;
+
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
     }
     return (
         <Flex align="center" justify="start" direction='column' fontSize='2rem' h="90vh">
@@ -26,68 +38,108 @@ function CreateEventForm() {
                 </Flex>
             </Flex>
             <Flex direction='column' mt='20px'>
-            <Text fontSize='md' fontWeight='bold'>Please enter the below details to create an event</Text>
-            <Flex align="center" justify="start" mt='40px' direction='row' fontSize='2rem'>
-                <form onSubmit={handleSubmit}>
-                    <Flex justify='center' alignItems="center">
-                        <FormControl isRequired mt="8px">
-                            <FormLabel>Event Name</FormLabel>
-                        </FormControl>
-                        <FormControl isRequired>
-                            <Input width='320' type="email" placeholder="Enter the event name" />
-                        </FormControl>
-                    </Flex>
-                    <Flex justify='center' alignItems="center">
-                        <FormControl isRequired mt="8px">
-                            <FormLabel>Description</FormLabel>
-                        </FormControl>
-                        <FormControl isRequired>
-                            <Input width='320' type="email" placeholder="Describe the event" />
-                        </FormControl>
-                    </Flex>
-                    <Flex justify='center' alignItems="center">
-                        <FormControl isRequired mt="8px">
-                            <FormLabel>Location</FormLabel>
-                        </FormControl>
-                        <FormControl isRequired>
-                            <Input width='320' type="email" placeholder="Enter the location" />
-                        </FormControl>
-                    </Flex>
-                    <Flex justify='center' alignItems="center">
-                        <FormControl isRequired mt="8px">
-                            <FormLabel>Club Name</FormLabel>
-                        </FormControl>
-                        <FormControl isRequired>
-                            <Input width='320' type="email" placeholder="Enter the club name" />
-                        </FormControl>
-                    </Flex>
-                    <Flex justify='center' alignItems="center">
-                        <FormControl isRequired mt="8px">
-                            <FormLabel>Event start Date and Time</FormLabel>
-                        </FormControl>
-                        <FormControl isRequired>
-                            <Input width='320' type="email" placeholder="Enter event start Date and Time" />
-                        </FormControl>
-                    </Flex>
-                    <Flex justify='center' alignItems="center">
-                        <FormControl isRequired mt="8px">
-                            <FormLabel>Event end Date and Time</FormLabel>
-                        </FormControl>
-                        <FormControl isRequired>
-                            <Input width='320' type="email" placeholder="Enter event end Date and Time" />
-                        </FormControl>
-                    </Flex>
-                    <Flex justify='center' alignItems="center">
-                        <FormControl isRequired mt="8px">
-                            <FormLabel>Organizer Email ID</FormLabel>
-                        </FormControl>
-                        <FormControl isRequired>
-                            <Input width='320' type="email" placeholder="Enter organizer's email ID" />
-                        </FormControl>
-                    </Flex>
-                    <Button mt='40px' type="submit" onSubmit={handleSubmit}>Submit</Button>
-                </form>
-            </Flex>
+                <Text fontSize='md' fontWeight='bold'>Please enter the below details to create an event</Text>
+                <Flex align="center" justify="start" mt='40px' direction='row' fontSize='2rem'>
+                    <form onSubmit={handleSubmit}>
+                        <Flex justify='center' alignItems="center">
+                            <FormControl isRequired mt="8px">
+                                <FormLabel>Event Name</FormLabel>
+                            </FormControl>
+                            <FormControl isRequired>
+                                <Input
+                                    onChange={(event) => {
+                                        setEventData({
+                                            ...eventData,
+                                            eventName: event.target.value
+                                        })
+                                    }}
+                                    width='320'
+                                    type="email"
+                                    placeholder="Enter the event name"
+                                />
+                            </FormControl>
+                        </Flex>
+                        <Flex justify='center' alignItems="center">
+                            <FormControl isRequired mt="8px">
+                                <FormLabel>Description</FormLabel>
+                            </FormControl>
+                            <FormControl isRequired>
+                                <Input onChange={(event) => {
+                                    setEventData({
+                                        ...eventData,
+                                        description: event.target.value
+                                    })
+                                }} width='320' type="email" placeholder="Describe the event" />
+                            </FormControl>
+                        </Flex>
+                        <Flex justify='center' alignItems="center">
+                            <FormControl isRequired mt="8px">
+                                <FormLabel>Location</FormLabel>
+                            </FormControl>
+                            <FormControl isRequired>
+                                <Input onChange={(event) => {
+                                    setEventData({
+                                        ...eventData,
+                                        location: event.target.value
+                                    })
+                                }} width='320' type="email" placeholder="Enter the location" />
+                            </FormControl>
+                        </Flex>
+                        <Flex justify='center' alignItems="center">
+                            <FormControl isRequired mt="8px">
+                                <FormLabel>Club Name</FormLabel>
+                            </FormControl>
+                            <FormControl isRequired>
+                                <Input onChange={(event) => {
+                                    setEventData({
+                                        ...eventData,
+                                        clubName: event.target.value
+                                    })
+                                }} width='320' type="email" placeholder="Enter the club name" />
+                            </FormControl>
+                        </Flex>
+                        <Flex justify='center' alignItems="center">
+                            <FormControl isRequired mt="8px">
+                                <FormLabel>Event start Date and Time</FormLabel>
+                            </FormControl>
+                            <FormControl isRequired>
+                                <Input onChange={(event) => {
+                                    setEventData({
+                                        ...eventData,
+                                        startDate: event.target.value
+                                    })
+                                }} width='320' type="email" placeholder="Enter event start Date and Time" />
+                            </FormControl>
+                        </Flex>
+                        <Flex justify='center' alignItems="center">
+                            <FormControl isRequired mt="8px">
+                                <FormLabel>Event end Date and Time</FormLabel>
+                            </FormControl>
+                            <FormControl isRequired>
+                                <Input onChange={(event) => {
+                                    setEventData({
+                                        ...eventData,
+                                        startTime: event.target.value
+                                    })
+                                }} width='320' type="email" placeholder="Enter event end Date and Time" />
+                            </FormControl>
+                        </Flex>
+                        <Flex justify='center' alignItems="center">
+                            <FormControl isRequired mt="8px">
+                                <FormLabel>Organizer Email ID</FormLabel>
+                            </FormControl>
+                            <FormControl isRequired>
+                                <Input onChange={(event) => {
+                                    setEventData({
+                                        ...eventData,
+                                        email: event.target.value
+                                    })
+                                }} width='320' type="email" placeholder="Enter organizer's email ID" />
+                            </FormControl>
+                        </Flex>
+                        <Button mt='40px' type="submit" onSubmit={handleSubmit}>Submit</Button>
+                    </form>
+                </Flex>
 
             </Flex>
         </Flex>
