d75259df699a320e2c74d609c8c1f536a49a1f41
preetisharmadal <preeti28062000@gmail.com>
2023-07-27 18:32:44 -0300

    added review new club request page
    Signed-off-by: preetisharmadal <preeti28062000@gmail.com>

--- frontend/src/Pages/ReviewNewClubRequest.js
+++ frontend/src/Pages/ReviewNewClubRequest.js
@@ -1,15 +1,45 @@
-import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Flex, Text } from '@chakra-ui/react';
-import React from 'react';
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Flex, Text, Toast } from '@chakra-ui/react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 
 
-function NewClubRequest() {
+function ReviewNewClubRequest() {
+
+  const [newClubRequest, setNewClubRequest] = useState({
+    name: "",
+    firstName: "",
+    lastName: "",
+    emailID: "",
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(`/registerClub`, JSON.stringify(newClubRequest));
+      if (response.status === 200) {
+        Toast({
+          title: 'Successful',
+          description: 'Your submission was successful.',
+          status: 'success',
+          duration: 5000,
+          isClosable: true,
+        });
+      }
+      return response.data;
+
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
   return (
     <Flex align="center" justify="start" direction='column' fontSize='2rem' h="90vh">
       <Flex alignItems="end" justifyContent="center" h="20vh" bgColor='black' width='100%' >
         <Flex h="18vh" bgColor='white' width='60%' alignItems='center' justifyContent='center'>
           <Flex h='10vh' bgColor='#ECC94B' width='40%' justifyContent='center' alignItems='center'>
-            <Text fontSize='2xl' fontWeight='extrabold'> New Club Requests</Text>
+            <Text fontSize='2xl' fontWeight='extrabold'> New Club Request</Text>
           </Flex>
         </Flex>
       </Flex>
@@ -39,8 +69,8 @@ function NewClubRequest() {
                     <Text fontSize='md'>Club Description:</Text>
                   </Flex>
                   <Flex width='80%'>
-                    <Text fontSize='md'>A Green values common interest society, pertaining to sustainability, social justice and grassroots democracy. 
-                    Our society brings together people with common interest in green values for discussions and events pertaining to these values</Text>
+                    <Text fontSize='md'>A Green values common interest society, pertaining to sustainability, social justice and grassroots democracy.
+                      Our society brings together people with common interest in green values for discussions and events pertaining to these values</Text>
                   </Flex>
                 </Flex>
                 <Flex direction='row' justifyContent='space-between'>
@@ -85,4 +115,4 @@ function NewClubRequest() {
   );
 }
 
-export default NewClubRequest;
+export default ReviewNewClubRequest;
