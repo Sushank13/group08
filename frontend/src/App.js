e9761eb659f176fd03a7027c7f88c0b2eced9a9a
Zainuddin Saiyed <zn561051@dal.ca>
2023-07-27 17:52:09 -0300

    Adding Club Recommender page with API integration, and Routing.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import AdminHomePage from './Pages/AdminHomePage';
 import PersistLogin from './Components/PersistLogin';
 import FindAllEvents from './Pages/FindAllEvents';
 import EventPage from './Pages/EventPage';
+import RecommenderPage from './Pages/RecommenderPage';
 
 const ROLES = {
   'member': 'member',
@@ -35,6 +36,8 @@ function App() {
 
           <Route path="FindClubs" element={<FindAllClubs />} />
           <Route path="club/:clubName" element={<ClubPage />} />
+          
+          <Route path="recommend" element={<RecommenderPage />} />
 
           {/* we want to protect these routes */}
           <Route element={<PersistLogin />}>
