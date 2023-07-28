ee10507e9cdd6f6fe3fbf6a3527e18c6145f3922
Om Anand <om.anand@dal.ca>
2023-07-28 03:13:51 -0300

    [Test] Added random generators

--- backend/src/test/java/testUtils/RandomGenerator.java
+++ backend/src/test/java/testUtils/RandomGenerator.java
@@ -1,9 +1,12 @@
 package testUtils;
 
+import com.dal.cs.backend.Club.ClassObject.Club;
+import com.dal.cs.backend.Event.EventObject.Event;
 import com.dal.cs.backend.member.Enum.MemberType;
 import com.dal.cs.backend.member.MemberObject.Member;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Random;
 
 public class RandomGenerator {
@@ -21,6 +24,7 @@ public class RandomGenerator {
         }
         return sb;
     }
+
     public static String generateRandomString(int length) {
         String randomCharacters = CHARACTERS + DIGITS;
         return generateRandomStringFromCharacters(length, randomCharacters).toString();
@@ -29,6 +33,11 @@ public class RandomGenerator {
     public static Integer generateRandomInteger(int upperBound) {
         Random random = new Random();
         return random.nextInt(1, upperBound);
+    }
+
+    public static Integer generateRandomInteger(int lowerBound, int upperBound) {
+        Random random = new Random();
+        return random.nextInt(lowerBound, upperBound);
     }
 
     public static String generateRandomPhoneNumber() {
@@ -49,8 +58,35 @@ public class RandomGenerator {
         int randomYear = generateRandomInteger(LocalDate.now().getYear());
         int randomMonth = generateRandomInteger(LocalDate.now().getMonthValue());
         int randomDate = generateRandomInteger(LocalDate.now().getDayOfMonth());
-        return LocalDate.of(randomYear,randomMonth, randomDate);
+        return LocalDate.of(randomYear, randomMonth, randomDate);
     }
+
+    public static LocalTime generateRandomTime() {
+        int randomHour = generateRandomInteger(LocalTime.now().getHour());
+        int randomMinute = generateRandomInteger(LocalTime.now().getMinute());
+        return LocalTime.of(randomHour, randomMinute);
+    }
+
+    public static String generateRandomClubID() {
+        return "CLB_" + generateRandomInteger(1000, 10000);
+    }
+
+    public static String generateRandomCategoryID() {
+        return "CAT_" + generateRandomInteger(1000, 10000);
+    }
+
+    public static String generateRandomEventID() {
+        return "EVNT_" + generateRandomInteger(1000, 10000);
+    }
+
+    private static String generateRandomFacebookLink() {
+        return "https://www.facebook.com/" + generateRandomString(10);
+    }
+
+    private static String generateRandomInstagramLink() {
+        return "https://www.instagram.com/" + generateRandomString(10);
+    }
+
 
     public static Member generateRandomDalClubMember() {
         String email = RandomGenerator.generateRandomEmail();
@@ -61,7 +97,42 @@ public class RandomGenerator {
         String mobile = generateRandomPhoneNumber();
         LocalDate date = generateRandomDate();
         String password = generateRandomString(10);
-        Member  newMember = new Member(email, firstName, lastName, MemberType.member, program, term, mobile, date, password);
+        Member newMember = new Member(email, firstName, lastName, MemberType.member, program, term, mobile, date, password);
         return newMember;
+    }
+
+    public static Member generateRandomAdminMember() {
+        Member admin = generateRandomDalClubMember();
+        admin.setMemberType(MemberType.admin);
+        return admin;
+    }
+
+    public static Member generateRandomPresidentMember() {
+        Member president = generateRandomDalClubMember();
+        president.setMemberType(MemberType.president);
+        return president;
+    }
+
+    public static Club generateRandomClub(String presidentEmailID, String clubID, String categoryID, String categoryName) {
+        String clubName = generateRandomString(10);
+        String description = generateRandomString(100);
+        String facebookLink = generateRandomFacebookLink();
+        String instagramLink = generateRandomInstagramLink();
+        String location = generateRandomString(100);
+        String meetingTime = generateRandomString(10);
+        String rules = generateRandomString(100);
+        return new Club(clubID, clubName, description, presidentEmailID, facebookLink, instagramLink, categoryID, location, meetingTime, null, rules, categoryName);
+    }
+
+    public static Event generateRandomEvent(String organiserEmailID, String clubID, String eventID) {
+        String eventName = generateRandomString(10);
+        String description = generateRandomString(100);
+        String venue = generateRandomString(100);
+        LocalDate startDate = generateRandomDate();
+        LocalDate endDate = generateRandomDate();
+        LocalTime startTime = generateRandomTime();
+        LocalTime endTime = generateRandomTime();
+        String eventTopic = generateRandomString(20);
+        return new Event(eventID, clubID, organiserEmailID, eventName, description, venue, null, startDate.toString(), endDate.toString(), startTime.toString(), endTime.toString(), eventTopic);
     }
 }
